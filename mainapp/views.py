bcc2db998728ef8e80297939a5f9a5fed51a2804
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-11-03 17:47:17 +0500

    cart

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, View
 
-from .models import Winter, Summer, Category, LatestProducts
+from .models import Winter, Summer, Category, LatestProducts, Customer, Cart
 from .mixins import CategoryDetailMixin
 
 
@@ -51,3 +51,15 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     #     context = super().get_context_data(**kwargs)
     #     context['cart'] = self.cart
     #     return context
+
+
+class CartView(View):
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
